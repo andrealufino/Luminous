12f3e7fdb0ae807f85c05dec8d51447806fadf90
Andrea Mario Lufino <andrea.lufino@me.com>
2020-11-30 14:19:33 +0100

    Fixed folder structure for SPM

--- Sources/Luminous/Luminous.swift
+++ Sources/Luminous/Luminous.swift
@@ -1,3 +1,52 @@
-struct Luminous {
-    var text = "Hello, World!"
+//
+//  Luminous.swift
+//  Pods
+//
+//  Created by Andrea Mario Lufino on 20/10/16.
+//
+//
+
+import AVFoundation
+import CoreMotion
+import CoreTelephony
+import ExternalAccessory
+import Foundation
+import SystemConfiguration.CaptiveNetwork
+
+import Deviice
+
+
+// MARK: Luminous
+
+public struct Luminous {
+    
+    
+    // MARK: Types
+    
+    /// The battery state.
+    ///
+    /// - unknown:   State unknown.
+    /// - unplugged: Battery is not plugged in.
+    /// - charging:  Battery is charging.
+    /// - full:      Battery is full charged.
+    public enum BatteryState: Int {
+        case unknown
+        case unplugged
+        case charging
+        case full
+    }
+    
+    /// The size scale to decide how you want to obtain size information.
+    ///
+    /// - bytes:     In bytes.
+    /// - kilobytes: In kilobytes.
+    /// - megabytes: In megabytes.
+    /// - gigabytes: In gigabytes.
+    public enum MeasureUnit {
+        case bytes
+        case kilobytes
+        case megabytes
+        case gigabytes
+        case percentage
+    }
 }
