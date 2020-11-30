2b9d0f7caf8158a47e61da9131481a1d0a9c7291
Andrea Mario Lufino <andrea.lufino@me.com>
2020-11-30 14:58:24 +0100

    Added .git to repo in dependecies

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
-            url: "https://github.com/andrealufino/Deviice",
+            url: "https://github.com/andrealufino/Deviice.git",
             .branch("master")
         )
     ],
