15906d5b3f5e8520c1137039183c0c9b36fc2eb5
Andrea Mario Lufino <andrea.lufino@me.com>
2020-11-30 15:35:26 +0100

    Fixes to make SPM work

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/andrealufino/Deviice.git",
-            .branch("master")
+            from: "1.30.2"
         )
     ],
     targets: [
