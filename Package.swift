272ee9e1ad3cbe2235899712c4264da8875f7ac6
Andrea Mario Lufino <andrea.lufino@me.com>
2020-11-30 15:38:49 +0100

    Updated path for target in SPM

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Luminous",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "LuminousTests",
             dependencies: ["Luminous"]),
